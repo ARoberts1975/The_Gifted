683ffd9010d27b1fc24d6bab9f2659cc8afc74ee
Allen <allenroberts1975@gmail.com>
2017-04-22 05:42:05 -0400

    Add files via upload

---
+++ tgthree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include "tgmenu.h"
+
+using namespace std;
+
+int tgthree()
+{
+
+string h = "The Gifted    -    Chapter 3    -    Page ";		// Chapter Header
+string p = "Chapter 3    -    Page ";						// Page header
+string t = "    ";											// 4 space tab
+int n = 0;
+
+	system ("cls");
+	cout << h << n++ << "\n" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	system ("pause");
+	tgmenu();
+	return 0;
+}
